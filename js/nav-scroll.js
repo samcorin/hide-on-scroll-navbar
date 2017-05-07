7e6bdb25bf86b0f6b78c0b7e0f1be42b9a341e1d
Sam Corin <samcorin@gmail.com>
2017-05-07 22:07:40 +0900

    removed bootstrap dependency

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -1,54 +1,27 @@
-// Hide Header on on scroll down
+//     if (st > lastScrollTop && st > navbarHeight){
+//         $('nav').removeClass('nav-down').addClass('nav-up', 450);
+//     } else {
+//         if(st + $(window).height() < $(document.body).height()) {
+//             $('nav').removeClass('nav-up').addClass('nav-down', 450);
+//
 $(document).ready(function() {
-  var didScroll;
-  var lastScrollTop = 0;
-  var delta = 5;
-  var navbarHeight = $('nav').outerHeight();
+  var beforeScroll = 0;
+  var afterScroll;
+  var $navbar = $('.navbar-wrapper');
 
-  $(document.body).scroll(function(event){
-    didScroll = true;
+  // current postion
+  $(window).on("scroll", function(){
+    afterScroll = $(this).scrollTop();
+
+    console.log(beforeScroll, afterScroll);
+
+    if (beforeScroll < afterScroll && afterScroll > 60) {
+      // Scrolling down
+      $navbar.removeClass('slideDown').addClass('slideUp');
+    } else if (beforeScroll > afterScroll) {
+      // Scrolling up
+      $navbar.removeClass('slideUp').addClass('slideDown');
+    }
+    beforeScroll = afterScroll;
   });
-
-  setInterval(function() {
-    if (didScroll) {
-      hasScrolled();
-      didScroll = false;
-    }
-  }, 150);
-
-  // if($(window.location.hash).length > 0){
-  //   $('html, body').animate({ scrollTop: $(window.location.hash).offset().top}, 1000);
-  // }
-
-  function hasScrolled() {
-    var st = $(window).scrollTop();
-    // var st = $('#outer').scrollTop();
-    console.log(this)
-
-    // Change background color / add class / remove class
-    // if (st > 500) {
-    //   $('nav').addClass('header-blue', 300);
-    // } else {
-    //   $('nav').removeClass('header-blue', 300);
-    // }
-
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-      console.log("scroll down");
-        // Scroll Down
-        $('nav').removeClass('nav-down').addClass('nav-up', 450);
-    } else {
-        console.log("scroll up");
-        // Scroll Up
-        if(st + $(window).height() < $(document.body).height()) {
-            $('nav').removeClass('nav-up').addClass('nav-down', 450);
-        }
-    }
-    lastScrollTop = st;
-  }
 });
